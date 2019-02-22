33544fa27e712e32a6c84b083185a932e851af87
lsb1918 <39327697+lsb1918@users.noreply.github.com>
2019-02-23 03:56:46 +0900

    캐릭터 이미지 사이즈 변경

--- js/Scene/CharSelectScene.js
+++ js/Scene/CharSelectScene.js
@@ -1,7 +1,20 @@
 var CharSelectScene = new function(){
+    var characters = [];
 
     return{
         init: function(onLoad) {
+            characters = new Array();
+            characters[0] = new Bebe();
+            characters[1] = new Kaze();
+            characters[2] = new Luga();
+            characters[3] = new Onnul();
+            characters[4] = new Rein();
+            characters[5] = new Wari();
+
+            for(var i = 0; i < characters.length; i++){
+                characters[i].init();
+            }
+            onLoad();
         },
         start: function(onLoad) {
             onLoad();
@@ -10,13 +23,25 @@ var CharSelectScene = new function(){
             UIManager.repaint();
         },
         render: function() {
-            ctx.font = "bold 40px malgun gothic";
-            ctx.fillText("캐릭터 선택화면", 340, 270);
-
             ctx.font = "bold 20px malgun gothic";
             ctx.fillText("ESC : 뒤로가기", 20, 50);
+
+            ctx.font = "bold 30px malgun gothic";
+            ctx.fillText("캐릭터를 선택해주세요.", 320, 100);
+
+            ctx.font = "bold 20px malgun gothic";
+            for(var i = 0; i < 3; i++ ){
+                characters[i].render(150 + (270 * i), 130);
+                ctx.fillText(characters[i].name, 180 + (270 * i), 285);
+            }
+
+            for(var i = 3; i < 6; i++ ){
+                characters[i].render(150 + (270 * (i - 3)), 330);
+                ctx.fillText(characters[i].name, 180 + (270 * (i - 3)), 490);
+            }
         },
         destroy: function(callback) {
+            characters = null;
             callback();
         },
         keyPressed: function(e) {
