295c9bcddafb87f9f33a63cc6998eb1a90b2f0ee
lsb1918 <39327697+lsb1918@users.noreply.github.com>
2019-02-24 00:12:38 +0900

    로딩씬

--- js/Scene/LoadingScene.js
+++ js/Scene/LoadingScene.js
@@ -1,12 +1,35 @@
 var LoadingScene = new function(){
-    
+    var loadingImg;
+    var loadingWidth;
+    var charObj;
 
     return{
-        init: function() {},
-        start: function() {},
-        render: function() {},
-        destroy: function() {},
-        keyPressed: function() {},
+        init: function(onLoad, data) {
+            charObj = data;
+            loadingWidth = 20;
+            Util.imgLoad(loadingImg = new Image(), "resource/loading.png", onLoad);
+        },
+        start: function(onLoad) {
+            onLoad();
+        },
+        update: function(){
+            if(loadingWidth < 350) loadingWidth += 10;
+            UIManager.repaint();
+        },
+        render: function() {
+            ctx.drawImage(loadingImg, 290, 30);
+            
+            ctx.strokeStyle = "#000000";
+            ctx.lineWidth = 1;
+            ctx.strokeRect(320, 450, 350, 20);
+
+            ctx.fillStyle = "#5CD1E5";
+            ctx.fillRect(320, 450, loadingWidth, 20);
+        },
+        destroy: function(callback) {
+            callback();
+        },
+        keyPressed: function(e) {},
         toString: function() {
             return "LoadingScene";
         }
