77802f5aec1af500c238dcf2a678907eb9ef6ac2
lsb1918 <39327697+lsb1918@users.noreply.github.com>
2019-02-23 23:37:39 +0900

    캐릭터씬 포커스 추가

--- js/Scene/CharSelectScene.js
+++ js/Scene/CharSelectScene.js
@@ -1,8 +1,17 @@
+//TODO. 페이징 추가 예정
 var CharSelectScene = new function(){
     var characters = [];
+    var curFocusIdx;
+    var focusX;
+    var focusY;
 
     return{
         init: function(onLoad) {
+            curFocusIdx = 0;
+            rectCount = 0;
+            focusX = [130, 400, 670, 130, 400, 670];
+            focusY = [120, 120, 120, 320, 320, 320];
+
             characters = new Array();
             characters[0] = new Bebe();
             characters[1] = new Kaze();
@@ -20,6 +29,7 @@ var CharSelectScene = new function(){
             onLoad();
         },
         update: function(){
+            rectCount++;
             UIManager.repaint();
         },
         render: function() {
@@ -39,22 +49,39 @@ var CharSelectScene = new function(){
                 characters[i].render(150 + (270 * (i - 3)), 330);
                 ctx.fillText(characters[i].name, 180 + (270 * (i - 3)), 490);
             }
+
+            ctx.strokeStyle = "#FF0000";
+            ctx.lineWidth = 6;
+            ctx.lineJoin = "round";
+            ctx.strokeRect(focusX[curFocusIdx], focusY[curFocusIdx], 140, 180);
         },
         destroy: function(callback) {
             characters = null;
+            curFocusIdx = null;
+            focusX = null;
+            focusY = null;
             callback();
         },
         keyPressed: function(e) {
             switch(e.keyCode){
                 case KEY_LEFT:
+                    if(curFocusIdx > 0) curFocusIdx--;
+                    else curFocusIdx = 5;
                     break;
                 case KEY_RIGHT:
+                    if(curFocusIdx < 5) curFocusIdx++;
+                    else curFocusIdx = 0;
                     break;
                 case KEY_UP:
+                    if(curFocusIdx < 3) curFocusIdx += 3;
+                    else curFocusIdx -= 3;
                     break;
                 case KEY_DOWN:
+                    if(curFocusIdx < 3) curFocusIdx += 3;
+                    else curFocusIdx -= 3;
                     break;
                 case KEY_ENTER:
+                    UIManager.changeScene(LoadingScene, characters[curFocusIdx]);
                     break;
                 case KEY_PREV:
                     UIManager.changeScene(TitleScene);
